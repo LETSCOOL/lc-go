84baa312488f9e665b34f3790cc700fcc61d9a0f
Yuchi <yuchi518@gmail.com>
2022-08-28 16:02:58 +0800

    Add version structure.

--- lg/gen_version_env.sh
+++ lg/gen_version_env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # 執行的目錄與與產生的 env 檔案相同
 
 COMMIT_TAGS=$(git tag --contains)
@@ -11,14 +11,14 @@ fi
 for COMMIT_TAG in $COMMIT_TAGS
 do
   #echo "check $COMMIT_TAG"
-  if [[ "$COMMIT_TAG" =~ ^v[0-9]+\.[0-9]+$ ]]
+  if [[ "$COMMIT_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
   then
     #echo "good"
     break
   fi
 done
 
-if ! [[ "$COMMIT_TAG" =~ ^v[0-9]+\.[0-9]+$ ]]
+if ! [[ "$COMMIT_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
 then
   echo "Incorrect tag format. ($COMMIT_TAG)"
   echo "All tags: $COMMIT_TAGS"
@@ -29,8 +29,10 @@ VERSION=$(echo $COMMIT_TAG | tr "v" ".")
 #echo $VERSION
 MAJOR=$(echo $VERSION | cut -d. -f2)
 MINOR=$(echo $VERSION | cut -d. -f3)
+PATCH=$(echo $VERSION | cut -d. -f4)
 #echo "Major: $MAJOR"
 #echo "Minor: $MINOR"
+#echo "Patch: $PATCH"
 
 COMMIT_SHA=${CI_COMMIT_SHA:-$(git rev-parse HEAD)}
 if [ -z "$COMMIT_SHA" ]
@@ -46,13 +48,14 @@ COMMIT_SHORT_SHA=${COMMIT_SHA:0:8}
 BUILD_DATE_STR=`date +%y%m%d`
 #echo $BUILD_DATE_STR
 
-IMAGE_TAG="$MAJOR.$MINOR.$BUILD_DATE_STR-$COMMIT_SHORT_SHA"
+IMAGE_TAG="$MAJOR.$MINOR.$PATCH.$BUILD_DATE_STR-$COMMIT_SHORT_SHA"
 echo "Preparing image with tag name: $IMAGE_TAG"
 
 echo "# 這個檔案會自動產生，不需要編輯。\n# 有沒有commit也無所謂。" > image_version.env
 echo "IMAGE_TAG=${IMAGE_TAG}" >> image_version.env
 echo "IMAGE_VERSION_MAJOR=$MAJOR" >> image_version.env
 echo "IMAGE_VERSION_MINOR=$MINOR" >> image_version.env
+echo "IMAGE_VERSION_PATCH=$PATCH" >> image_version.env
 echo "IMAGE_COMMIT_SHORT_SHA=${COMMIT_SHORT_SHA}" >> image_version.env
 echo "IMAGE_BUILD_DATE=${BUILD_DATE_STR}" >> image_version.env
 
--- lg/ver.go
+++ lg/ver.go
@@ -1,6 +1,7 @@
 package lg
 
 import (
+	"errors"
 	"github.com/joho/godotenv"
 	"log"
 )
@@ -9,6 +10,7 @@ const (
 	EnvImageTag            = "IMAGE_TAG"
 	EnvImageVersionMajor   = "IMAGE_VERSION_MAJOR"
 	EnvImageVersionMinor   = "IMAGE_VERSION_MINOR"
+	EnvImageVersionPatch   = "IMAGE_VERSION_PATCH"
 	EnvImageCommitShortSha = "IMAGE_COMMIT_SHORT_SHA"
 	EnvImageBuildDate      = "IMAGE_BUILD_DATE"
 	EnvUnknownString       = "__unknown__"
@@ -17,20 +19,42 @@ const (
 var (
 	MajorVersion   = 0
 	MinorVersion   = 0
+	PatchVersion   = 0
 	ImageTag       = EnvUnknownString
 	CommitShortSHA = EnvUnknownString
 	ImageBuildDate = EnvUnknownString
 )
 
-func LoadVerEnv(filenames ...string) {
+// Version 表示程式碼實作的版本
+type Version struct {
+	Major          int    `json:"major"`
+	Minor          int    `json:"minor"`
+	Patch          int    `json:"patch"`
+	BuildDate      string `json:"buildDate"`
+	ImageTag       string `json:"imageTag"`
+	CommitShortSHA string `json:"commitShortSHA"`
+	Route          string `json:"route"`
+}
+
+func LoadVerEnv(filenames ...string) (*Version, error) {
 	err := godotenv.Load(filenames...)
 	if err != nil {
 		log.Println("Error loading image_version.env file")
+		return nil, errors.New("loading image_version.env file failed")
 	} else {
 		MajorVersion = GetenvInt(EnvImageVersionMajor, 0)
 		MinorVersion = GetenvInt(EnvImageVersionMinor, 0)
+		PatchVersion = GetenvInt(EnvImageVersionPatch, 0)
 		ImageTag = GetenvStr(EnvImageTag, EnvUnknownString)
 		CommitShortSHA = GetenvStr(EnvImageCommitShortSha, EnvUnknownString)
 		ImageBuildDate = GetenvStr(EnvImageBuildDate, EnvUnknownString)
+		return &Version{
+			Major:          MajorVersion,
+			Minor:          MinorVersion,
+			Patch:          PatchVersion,
+			BuildDate:      ImageBuildDate,
+			ImageTag:       ImageTag,
+			CommitShortSHA: CommitShortSHA,
+		}, nil
 	}
 }
